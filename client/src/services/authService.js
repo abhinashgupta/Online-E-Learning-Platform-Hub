3eacd61d2f73ed55c12d10e87bf0a26147a564b1
Abhinash Gupta <abhinashgupta1822@example.com>
2025-07-28 00:18:13 +0530

    frontend done

---
+++ client/src/services/authService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL + "/users/";
+
+const register = async (userData) => {
+  const response = await axios.post(API_URL + "register", userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+const logout = () => localStorage.removeItem("user");
+
+const authService = { register, login, logout };
+export default authService;
