cd0bc3deedd646ad92cc56541a0647ec36875046
Abhinash Gupta <abhinashgupta1822@example.com>
2025-08-06 02:05:12 +0530

    removed previos folders

--- client/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-function Spinner() {
-  return (
-    <div className="fixed top-0 right-0 bottom-0 left-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
-    </div>
-  );
-}
-
-export default Spinner;
